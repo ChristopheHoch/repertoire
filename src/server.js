55667e807e14ac5651098f9679a0f5a93d38f517
Christophe Hoch <christophe.hoch@gmail.com>
2013-10-06 21:13:49 +0000

    Server update with landing page

--- src/server.js
+++ src/server.js
@@ -3,8 +3,27 @@
 (function() {
    "use strict";
 
-   /**
-    * Module dependencies.
-    */
+   var express = require('express'),
+       http = require('http'),
+       path = require('path'),
+       config = require('./config'),
+       app = express();
+   
+   app.set('port', process.env.PORT || 3000);
+   app.use(express.logger('dev'));
+   app.use(express.json());
+   app.use(express.urlencoded());
+   app.use(express.methodOverride());
+   app.use(express.cookieParser());
+   app.use(express.session({ secret: 'The answer is 42' }));
+   app.use(express.static(path.join(__dirname, 'public')));
+   
+   if ('development' == app.get('env')) {
+      app.use(express.errorHandler());
+   }
+
+   http.createServer(app).listen(app.get('port'), function(){
+      console.log('Express server listening on port ' + app.get('port'));
+   });
 
 }());
