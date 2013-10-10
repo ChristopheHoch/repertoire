924c108d469cb6533dc8ca8e608c606c4376a987
Christophe Hoch <christophe.hoch@gmail.com>
2013-10-10 21:37:49 +0000

    Testing Mocha

--- src/server.js
+++ src/server.js
@@ -25,5 +25,7 @@
    http.createServer(app).listen(app.get('port'), function(){
       console.log('Express server listening on port ' + app.get('port'));
    });
+  
+   module.exports = app;
 
 }());
--- test/server.js
+++ test/server.js
@@ -2,35 +2,20 @@
    "use strict";
    
    var should = require('chai').should(),
+       app = require('../server.js'),
        config = require('../config.js'),
-       server = require('../server.js'),
-       port = process.env.PORT || 3000;
+       request = require('supertest');
 
    describe('server', function() {
-      
-      before(function(done) {
-         server.listen(port, function (err, result) {
-            if (err) {
-              done(err);
-            } else {
-              done();
+     
+      it('should have an index', function (done) {
+         request(app)
+         .get('http://www.google.fr/')
+         .expect(200)
+         .end(function(err, res) {
+            if(err) {
+               return done(err);
             }
-         });
-      });
-      
-      after(function(done) {
-         server.close();
-      });
-      
-      it('should exist', function (done) {
-         should.exist(app);
-         done();
-      });
-      
-      it('should be listening at localhost', function (done) {
-         var headers = defaultGetOptions('/');
-         http.get(headers, function (res) {
-            res.statusCode.should.eql(404);
             done();
          });
       });
