3e3705ab632251183539853b350e97862f72c514
Christophe Hoch <christophe.hoch@gmail.com>
2013-10-14 20:52:38 +0000

    Work on basic passport local strategy

---
+++ src/config-sample.js
@@ -0,0 +1,16 @@
+/**
+  * Config template, to be renamed to config.js before use.
+  */
+
+(function() {
+   "use strict";
+  
+   exports.db = {
+      mongodb: 'mongodb://localhost:27017/repertoire'
+   };
+   
+   exports.url = {
+      app: 'http://localhost:3000/'
+   };
+   
+}());
--- src/server.js
+++ src/server.js
@@ -6,7 +6,8 @@
    var express = require('express'),
        http = require('http'),
        path = require('path'),
-//       config = require('./config'),
+       passport = require('./authentication'),
+       config = require('./config'),
        app = express();
 
    module.exports = app;
@@ -18,7 +19,33 @@
    app.use(express.methodOverride());
    app.use(express.cookieParser());
    app.use(express.session({ secret: 'The answer is 42' }));
+   app.use(passport.initialize());
+   app.use(passport.session());
    app.use(express.static(path.join(__dirname, 'public')));
+   
+   app.get('/', ensureAuthenticated, function(req, res){
+      res.render('index', { user: req.user });
+   });
+
+   app.get('/login', function(req, res){
+      res.render('login', { user: req.user, message: req.flash('error') });
+   });
+   
+   function ensureAuthenticated(req, res, next) {
+      if (req.isAuthenticated()) {
+         return next();
+      }
+      res.redirect('/login.html');
+   }
+   
+   app.post('/login', passport.authenticate('local',
+      {
+         failureRedirect: '/login',
+         failureFlash: true
+      }),
+     function(req, res) {
+       res.redirect('/');
+    });
    
    if ('development' == app.get('env')) {
       app.use(express.errorHandler());
