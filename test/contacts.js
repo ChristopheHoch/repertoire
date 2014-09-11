ed745b690c3c75f982c053fc44c18cf7ddf1dca7
Christophe Hoch <christophe.hoch@gmail.com>
2014-09-11 07:39:25 +0200

    Add more test on /api/contacts/:id

--- test/contacts.js
+++ test/contacts.js
@@ -11,16 +11,22 @@ var app = require('../src/server'),
 describe('Cloud Repertoire contacts api', function () {
     'use strict';
     var userId,
-        contactId;
+        contactId,
+        userData = {
+            email: 'john@test.com',
+            password: 'doe'
+        },
+        contactData = {
+            first_name: 'John',
+            last_name: 'Doe',
+            email: 'john.doe@email.com'
+        };
 
     beforeEach(function (done) {
         /* Ajout d'un utilisateur */
         userId = undefined;
 
-        var user = new UserSchema({
-            email: 'john@test.com',
-            password: 'doe'
-        });
+        var user = new UserSchema(userData);
 
         user.save(function (error, savedUser) {
             userId = savedUser._id;
@@ -29,11 +35,7 @@ describe('Cloud Repertoire contacts api', function () {
         /* Ajout d'un contact */
         contactId = undefined;
 
-        var contact = new ContactSchema({
-            first_name: 'John',
-            last_name: 'Doe',
-            email: 'john.doe@email.com'
-        });
+        var contact = new ContactSchema(contactData);
 
         contact.save(function (error, savedContact) {
             contactId = savedContact._id;
@@ -74,14 +76,10 @@ describe('Cloud Repertoire contacts api', function () {
     describe('when requesting all contacts with a bearer token', function () {
 
         it('should respond with 401', function (done) {
-            var user = {
-                email: 'john@test.com',
-                password: 'doe'
-            };
 
             request(app)
                 .post('/authenticate')
-                .send(user)
+                .send(userData)
                 .expect('Content-Type', /json/)
                 .expect(200)
                 .end(function (error, doc) {
@@ -118,14 +116,10 @@ describe('Cloud Repertoire contacts api', function () {
     describe('when requesting a incorrect contact id', function () {
 
         it('should respond with 404', function (done) {
-            var user = {
-                email: 'john@test.com',
-                password: 'doe'
-            };
 
             request(app)
                 .post('/authenticate')
-                .send(user)
+                .send(userData)
                 .expect('Content-Type', /json/)
                 .expect(200)
                 .end(function (error, doc) {
@@ -151,14 +145,10 @@ describe('Cloud Repertoire contacts api', function () {
     describe('when requesting a non existing contact id', function () {
 
         it('should respond with 404', function (done) {
-            var user = {
-                email: 'john@test.com',
-                password: 'doe'
-            };
 
             request(app)
                 .post('/authenticate')
-                .send(user)
+                .send(userData)
                 .expect('Content-Type', /json/)
                 .expect(200)
                 .end(function (error, doc) {
@@ -174,6 +164,45 @@ describe('Cloud Repertoire contacts api', function () {
                         .get('/api/contacts/' + userId)
                         .set('authorization', bearerToken)
                         .expect(404, done);
+                });
+
+
+
+        });
+    });
+
+    describe('when requesting an existing contact id', function () {
+
+        it('should respond with 200', function (done) {
+
+            request(app)
+                .post('/authenticate')
+                .send(userData)
+                .expect('Content-Type', /json/)
+                .expect(200)
+                .end(function (error, doc) {
+                    var bearerToken;
+
+                    should.not.exist(error);
+                    should.exist(doc.body);
+                    should.exist(doc.body.token);
+
+                    bearerToken = 'Bearer ' + doc.body.token;
+
+                    request(app)
+                        .get('/api/contacts/' + contactId)
+                        .set('authorization', bearerToken)
+                        .expect(200)
+                        .end(function (error, doc) {
+                            var contact = doc.body;
+                            should.not.exist(error);
+                            should.exist(contact);
+                            should.exist(contact._id);
+                            contact.first_name.should.equal(contactData.first_name);
+                            contact.last_name.should.equal(contactData.last_name);
+                            contact.email.should.equal(contactData.email);
+                            done();
+                        });
                 });
 
 
